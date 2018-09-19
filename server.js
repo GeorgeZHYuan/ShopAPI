a0bd3f4df53adc4f2390cbb260995617749cd832
George Yuan <georgeyuan@v1020-wn-48-139.campus-dynamic.uwaterloo.ca>
2018-09-19 10:22:41 -0400

    added routes templates

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const lineitems = require('./routes/api/lineitems');
+const orders = require('./routes/api/orders');
+const orderitems = require('./routes/api/orderitems');
+const accounts = require('./routes/api/accounts');
+const shops = require('./routes/api/shops');
+const transactions = require('./routes/api/transactions');
 
 const app = express();
 
@@ -7,5 +15,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
+
+app.use('api/v1/lineitems', lineitems);
+app.use('api/v1/orders', orders);
+app.use('api/v1/orderitems', orderitems);
+app.use('api/v1/accouts', accounts);
+app.use('api/v1/shops', shops);
+app.use('api/v1/transactions', transactions);
+
+app.get('/', (err, res) => {
+    res.send('<h1>Shopify Developer Challenge API</h1>');
+});
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
