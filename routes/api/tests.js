f50dd8a74735a0619fd573289cdab1d1ae03f0a8
George Yuan <georgeyuan@Georges-MacBook-Pro.local>
2018-09-22 16:34:10 -0400

    added google cloud db to routes

---
+++ routes/api/tests.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+
+const crypto = require('crypto');
+
+module.exports = (knex) => {
+    /**
+     * Insert a visit record into the database.
+     *
+     * @param {object} knex The Knex connection object.
+     * @param {object} visit The visit record to insert.
+     * @returns {Promise}
+     */
+    function insertVisit (knex, visit) {
+      return knex('visits').insert(visit);
+    }
+
+    /**
+     * Retrieve the latest 10 visit records from the database.
+     *
+     * @param {object} knex The Knex connection object.
+     * @returns {Promise}
+     */
+    function getVisits (knex) {
+      return knex.select('timestamp', 'userIp')
+        .from('visits')
+        .orderBy('timestamp', 'desc')
+        .limit(10)
+        .then((results) => {
+          return results.map((visit) => `Time: ${visit.timestamp}, AddrHash: ${visit.userIp}`);
+        });
+    }
+
+    router.get('/visits', (req, res, next) => {
+      // Create a visit record to be stored in the database
+      const visit = {
+        timestamp: new Date(),
+        // Store a hash of the visitor's ip address
+        userIp: crypto.createHash('sha256').update(req.ip).digest('hex').substr(0, 7)
+      };
+
+      insertVisit(knex, visit)
+        // Query the last 10 visits from the database.
+        .then(() => getVisits(knex))
+        .then((visits) => {
+          res
+            .status(200)
+            .set('Content-Type', 'text/plain')
+            .send(`Last 10 visits:\n${visits.join('\n')}`)
+            .end();
+        })
+        .catch((err) => {
+          next(err);
+        });
+    });
+
+    router.get('/accounts', (req, res, next) => {
+        knex.select('*')
+            .from('accounts')
+            .then((results) => {
+                res.send(results);
+            });
+    });
+
+
+
+    return router;
+}
--- server.js
+++ server.js
@@ -1,31 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Routes
 const lineitems = require('./routes/api/lineitems');
 const orders = require('./routes/api/orders');
 const orderitems = require('./routes/api/orderitems');
 const accounts = require('./routes/api/accounts');
 const shops = require('./routes/api/shops');
 const transactions = require('./routes/api/transactions');
+const tests = require('./routes/api/tests');
 
+// Server
 const app = express();
+const port = process.env.PORT || 5000;
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
+// Google cloud DB
+const knex = require('./db/googleCloudDB');
 
-app.use('api/v1/lineitems', lineitems);
-app.use('api/v1/orders', orders);
-app.use('api/v1/orderitems', orderitems);
-app.use('api/v1/accouts', accounts);
-app.use('api/v1/shops', shops);
-app.use('api/v1/transactions', transactions);
+// Ues routes
+app.use('/api/v1/lineitems', lineitems(knex));
+app.use('/api/v1/orders', orders(knex));
+app.use('/api/v1/orderitems', orderitems(knex));
+app.use('/api/v1/accouts', accounts(knex));
+app.use('/api/v1/shops', shops(knex));
+app.use('/api/v1/transactions', transactions(knex));
+app.use('/api/v1/tests', tests(knex));
 
 app.get('/', (err, res) => {
     res.send('<h1>Shopify Developer Challenge API</h1>');
 });
-
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
