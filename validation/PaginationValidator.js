58c657b95bd55bb4233ff4c5f84c0a40ff8aaed4
George Yuan <georgeyuan@Georges-MacBook-Pro.local>
2018-09-23 22:39:14 -0400

    cleaned up code and added comments

--- validation/PaginationValidator.js
+++ validation/PaginationValidator.js
@@ -4,17 +4,19 @@ const isEmpty = require('./isEmpty');
 module.exports = function validatePaginationParams(query) {
     let errors = {};
 
+    // Check sort order
     if (query.sort !== 'asc' || query.sort !== 'desc') {
         errors.sort == `invalid sorting method. choose between 'asc' and 'desc'.`;
     }
 
+    // Check pagination limit
     if (!Validator.isInt(query.limit)) {
-        console.log('Number', query.limit);
         errors.limit = `Invalid paginiation limit. Limit must be an integer.`;
     } else if (query.limit < 1) {
         errors.limit = `Invalid paginiation limit. Limit must be a positive integer.`;
     }
 
+    // Check pagination offset
     if (!Validator.isInt(query.offset)) {
         errors.offset = `Invalid paginiation offset. Offset must be an integer.`;
     } else if (query.offset < 1) {
